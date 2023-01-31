549cc5ce0dfae20a312401f4433ea5f8a16731da
Kris <sanjayakresnawan@gmail.com>
2023-02-01 00:00:53 +0700

    upload file

---
+++ main.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+const bangkuP = JSON.parse(fs.readFileSync('bangku.json')).bangkuP;
+const bangkuL = JSON.parse(fs.readFileSync('bangku.json')).bangkuL;
+const arrSiswa = JSON.parse(fs.readFileSync('siswa.json')).siswa;
+
+function chooseRandom(gender) {
+    let chosen;
+    if (gender === "L") {
+        const randomNumber = Math.floor(Math.random() * bangkuL.length);
+        chosen = bangkuL[randomNumber];
+
+        bangkuL.splice(bangkuL.indexOf(chosen), 1);
+    } else if (gender === "P") {
+        const randomNumber = Math.floor(Math.random() * bangkuP.length);
+        chosen = bangkuP[randomNumber];
+
+        bangkuP.splice(bangkuP.indexOf(chosen), 1);
+    }
+    
+    return chosen;
+}
+
+for (var i = 0; i < arrSiswa.length; i++) {
+    const chosenBangku = chooseRandom(arrSiswa[i].gender);
+    const str = `nama: ${arrSiswa[i].nama}\n no_absen: ${arrSiswa[i].nomorAbsen}\n bangku: ${chosenBangku.kodeBangku}\n\n`
+
+    console.log(str);
+}
+
+// console.log(bangkuP.length);
