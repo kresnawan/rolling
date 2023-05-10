e35a5b47fe5f2c66d6be64c6bd945837f17527e9
Kris <sanjayakresnawan@gmail.com>
2023-05-10 07:15:45 +0700

    add socket io

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors")
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+app.use(cors({origin: "*"}))
 
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/index.html');
+    res.send('API successfully called!');
 });
 
 io.on('connection', (socket) =>{
@@ -17,6 +24,6 @@ io.on('connection', (socket) =>{
     })
 })
 
-server.listen(3000, () =>{
+server.listen(8000, () =>{
     console.log("Server successfully runned");
 });
